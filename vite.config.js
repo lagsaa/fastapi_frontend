713360047aa5a62de51f46ce862fa541f2bd279f
lagsaa <mingo.angelyn.olimba@gmail.com>
2025-04-21 19:14:42 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 // Change base to match your repo name
 export default defineConfig({
   plugins: [react()],
-  base: "/frontend/", 
+  base: "/fastapi_frontend/", 
 });
